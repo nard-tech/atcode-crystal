498aa90615e3d7bdbfc3d6ac0d28c46a41f9c810
Shu Fujita <s.fujita@nard.tech>
2020-02-22 17:19:54 +0900

    Add file of "Welcome to AtCoder"
    https://atcoder.jp/contests/practice/tasks/practice_1

---
+++ practice/practice_1.cr
@@ -0,0 +1,10 @@
+# Welcome to AtCoder
+# https://atcoder.jp/contests/practice/tasks/practice_1
+
+a = read_line.to_i64
+b, c = read_line.split.map(&.to_i64)
+s = read_line
+
+print a + b + c
+print ' '
+puts s
