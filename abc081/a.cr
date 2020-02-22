e72a7cd509b0bfea03a6bf3debd609daedc705b5
Shu Fujita <s.fujita@nard.tech>
2020-02-22 17:30:39 +0900

    [ABC 081 A] Placing Marbles - Add file
    https://atcoder.jp/contests/abc081/tasks/abc081_a

---
+++ abc081/a.cr
@@ -0,0 +1,7 @@
+# ABC 081 A - Placing Marbles
+# https://atcoder.jp/contests/abc081/tasks/abc081_a
+
+# cf. [AtCoder に登録したら解くべき精選過去問 10 問を Crystal で解いてみた](https://qiita.com/tomerun/items/23ce2a2ed6ead291d222)
+# 第2問
+
+puts read_line.chars.count('1')
