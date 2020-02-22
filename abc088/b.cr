b9428d721cce0d4b77dd2989c08aaa9a8bad1530
Shu Fujita <s.fujita@nard.tech>
2020-02-22 20:06:43 +0900

    [ABC 088 B] Card Game for Two - Add file
    https://atcoder.jp/contests/abc088/tasks/abc088_b

---
+++ abc088/b.cr
@@ -0,0 +1,19 @@
+# ABC 088 B - Card Game for Two
+# https://atcoder.jp/contests/abc088/tasks/abc088_b
+
+# cf. [AtCoder に登録したら解くべき精選過去問 10 問を Crystal で解いてみた](https://qiita.com/tomerun/items/23ce2a2ed6ead291d222)
+# 第6問
+
+n = read_line.to_i64
+array = read_line.split.map(&.to_i64)
+
+alice_cards = [] of Int64
+bob_cards = [] of Int64
+
+array.sort.reverse.each_slice(2) do |ary|
+  a, b = ary.map(&.to_i64)
+  alice_cards.push a
+  bob_cards.push b
+end
+
+puts alice_cards.sum - bob_cards.sum
---
+++ abc088/memo.md
@@ -0,0 +1,13 @@
+#   ABC 088
+
+##  B - Card Game for Two
+
+https://atcoder.jp/contests/abc088/tasks/abc088_b
+
+*   配列の生成
+
+```crystal
+[] of Int64
+```
+
+*   `each_slice` の挙動
