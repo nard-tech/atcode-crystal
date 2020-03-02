b1e5d5ed1c96f2669903d91aa30d7ef81b7d9ff2
Shu Fujita <s.fujita@nard.tech>
2020-03-03 04:10:15 +0900

    [ABC 127 A] Ferris Wheel - Fix file

--- abc127/a.cr
+++ abc127/a.cr
@@ -2,12 +2,12 @@
 # https://atcoder.jp/contests/abc127/tasks/abc127_a
 
 age, price_base = read_line.split.map(&.to_i64)
-if age >= 13
-  price = price_base
-elsif age >= 6
-  price = (price_base / 2).to_i64
-else
-  price = 0
-end
+price = if age >= 13
+          price_base
+        elsif age >= 6
+          (price_base / 2).to_i64
+        else
+          0
+        end
 
 puts price
