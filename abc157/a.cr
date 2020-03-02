c9b44cbf53d9d70167664ef3232182e74819cfec
Shu Fujita <s.fujita@nard.tech>
2020-03-03 04:51:55 +0900

    [ABC 157 A] Add comment

--- abc157/a.cr
+++ abc157/a.cr
@@ -1,6 +1,8 @@
 # ABC 157 A - Duplex Printing
 # https://atcoder.jp/contests/abc157/tasks/abc157_a
 
+# for Crystal 0.20.5
+
 n = read_line.to_i64
 n_papers = n.even? ? n / 2 : n / 2 + 1
 puts n_papers
