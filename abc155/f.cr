04daa7c6008fd646b3a07393330e58e0aebda399
Shu Fujita <s.fujita@nard.tech>
2020-03-03 22:39:23 +0900

    [ABC 155 F] Perils in Parallel - Add file

---
+++ abc155/f.cr
@@ -0,0 +1,5 @@
+# ABC 155 F - Perils in Parallel
+# https://atcoder.jp/contests/abc155/tasks/abc155_f
+n, m = read_line.split.map(&.to_i64)
+a_and_b = Array.new(n) { read_line.split.map(&.to_i64) }
+l_and_m = Array.new(m) { read_line.split.map(&.to_i64) }
