aa59a4000cab54d317e6be41f1028a66357fc0e2
Shu Fujita <s.fujita@nard.tech>
2020-03-03 11:30:19 +0900

    [ABC 155 C] Poll - Fix file

--- abc155/c.cr
+++ abc155/c.cr
@@ -3,7 +3,14 @@
 
 n = read_line.to_i64
 s = Array.new(n) { read_line }
-collection = s.tally
+collection = s.each_with_object(Hash(String, Int32).new) do |item, hash|
+  count = hash[item]?
+  if count
+    hash[item] = count + 1
+  else
+    hash[item] = 1
+  end
+end
 
 max = collection.values.max.to_i64 # Int64 とする
 collection.select { |k, v|
