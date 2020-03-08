d841bc69cc8378fd447c6f135bf911950b28b072
Shu Fujita <s.fujita@nard.tech>
2020-03-08 11:34:03 +0900

    [ABC 158 B] Fix file

--- abc158/b.cr
+++ abc158/b.cr
@@ -7,7 +7,7 @@ if a == 0
 elsif b == 0
   puts n
 else
-  n_loops = (n / (a + b)).floor.to_i64
-  # puts n_loops
-  puts n - n_loops * b
+  n_loops = (n / (a + b)).to_i64
+  r = (n % (a + b)).to_i64
+  puts n_loops * a + [a, r].min
 end
