376fcdee6abf94bfc455fe06da3e45180a0c65b3
Shu Fujita <s.fujita@nard.tech>
2020-03-08 12:09:36 +0900

    [ABC 154 D] Fix file (v4)

--- abc154/d.cr
+++ abc154/d.cr
@@ -24,7 +24,19 @@ p_pairs.each.with_index(0) do |p_pair, i|
 end
 
 def calc_max_sum(pairs : Array(Pair))
-  pairs.max_of(&.sum)
+  if pairs.size == 1
+    pairs.first.sum
+  else
+    last_index_of_first = (pairs.size - 1) / 2
+    first_index_of_last = last_index_of_first + 1
+    groups = [
+      pairs[0..last_index_of_first],
+      pairs[first_index_of_last..(pairs.size - 1)]
+    ] # Array(Array(Pair))
+    groups.map { |group|
+      calc_max_sum(group)
+    }.max
+  end
 end
 
 puts (calc_max_sum(pairs) + k) / 2.0
