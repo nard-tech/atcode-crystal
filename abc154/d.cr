ddd3e3f141fa3779241e8d44e956d3cdfedae5ce
Shu Fujita <s.fujita@nard.tech>
2020-03-08 11:45:59 +0900

    [ABC 154 D] Fix file (v3)

--- abc154/d.cr
+++ abc154/d.cr
@@ -4,5 +4,23 @@
 n, k = read_line.split.map(&.to_i64)
 p = read_line.split.map(&.to_i64)
 
-p_pairs = p.each_cons(k).to_a
-puts (p_pairs.map(&.sum).max + k) / 2.0
+class Pair
+  def initialize(array : Array(Int64), from : Int64)
+    @array = array
+    @from = from
+  end
+
+  getter :from
+
+  def sum
+    @array.sum
+  end
+end
+
+p_pairs = p.each_cons(k)
+pairs = [] of Pair
+p_pairs.each.with_index(0) do |p_pair, i|
+  pairs.push(Pair.new(p_pair, i.to_i64))
+end
+
+puts (pairs.max_of(&.sum) + k) / 2.0
