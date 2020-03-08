d6b7216ce0cc766d687442217b9129d843f69702
Shu Fujita <s.fujita@nard.tech>
2020-03-08 09:36:55 +0900

    [ABC 154 A] Remaining Balls - Add file

---
+++ abc154/a.cr
@@ -0,0 +1,11 @@
+# ABC 154 A - Remaining Balls
+# https://atcoder.jp/contests/abc154/tasks/abc154_a
+
+s, t = read_line.split
+a, b = read_line.split.map(&.to_i64)
+u = read_line
+if s == u
+  puts "#{a - 1} #{b}"
+elsif
+  puts "#{a} #{b - 1}"
+end
