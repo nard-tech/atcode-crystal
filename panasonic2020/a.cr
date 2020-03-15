c8c7fd246acf53e3feaab0a40f832a61040a549c
Shu Fujita <s.fujita@nard.tech>
2020-03-15 09:47:39 +0900

    [Panasonic 2020 A] Kth Term - Add file

---
+++ panasonic2020/a.cr
@@ -0,0 +1,6 @@
+# Panasonic 2020 A - Kth Term
+# https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_a
+
+k = read_line.to_i64
+array = [1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51]
+puts array[k - 1]
