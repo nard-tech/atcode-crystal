5088c0337914a1f5fcdeb986ed9b62d8465f1d15
Shu Fujita <s.fujita@nard.tech>
2020-03-19 20:21:27 +0900

    [ABC 151 C] Welcome to AtCoder - Add file (Ruby, WA)

--- abc151/c.rb
+++ abc151/c.rb
@@ -1,10 +1,10 @@
 # ABC 151 C - Welcome to AtCoder
 # https://atcoder.jp/contests/abc151/tasks/abc151_c
 
-n, m = read_line.split.map(&.to_i64)
-ps_array = Array.new(m) { p, s = read_line.split; [p.to_i64, s] }
+n, m = gets.split.map(&:to_i)
+ps_array = Array.new(m) { p, s = gets.split; [p.to_i, s] }
 
-groups = {} of Int64 => Array(String)
+groups = {}
 
 ps_array.each do |ps|
   p, s = ps
@@ -17,16 +17,15 @@ end
 
 puts groups
 
-acs = 0_i64
-was = 0_i64
+acs = 0
+was = 0
 
-groups.each do |problem, pss|
-  pss.each do |ps|
-    answer = ps[1]
+groups.each do |problem, answers|
+  answers.each do |answer|
     if answer == "WA"
-      was += 1_i64
+      was += 1
     elsif answer == "AC"
-      acs += 1_i64
+      acs += 1
       break
     end
   end
