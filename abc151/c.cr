dcd46132b5f32676bc31bc0e7e44bd296998a6eb
Shu Fujita <s.fujita@nard.tech>
2020-03-19 20:17:37 +0900

    [ABC 151 C] Welcome to AtCoder - Add file (Crystal, WA)

---
+++ abc151/c.cr
@@ -0,0 +1,24 @@
+# ABC 151 C - Welcome to AtCoder
+# https://atcoder.jp/contests/abc151/tasks/abc151_c
+
+n, m = read_line.split.map(&.to_i64)
+ps = Array.new(m) { p, s = read_line.split; [p.to_i64, s] }
+
+acs = 0_i64
+was = 0_i64
+
+groups = ps.group_by(&.first)
+
+groups.each do |problem, pss|
+  pss.each do |ps|
+    answer = ps[1]
+    if answer == "WA"
+      was += 1
+    elsif answer == "AC"
+      acs += 1
+      break
+    end
+  end
+end
+
+puts "#{acs} #{was}"
