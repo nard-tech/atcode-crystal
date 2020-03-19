255ecad338fcf519793b9596acd4c4b526dec11f
Shu Fujita <s.fujita@nard.tech>
2020-03-19 20:24:19 +0900

    [ABC 151 C] Fix file (Ruby, WA)

--- abc151/c.rb
+++ abc151/c.rb
@@ -1,7 +1,7 @@
 # ABC 151 C - Welcome to AtCoder
 # https://atcoder.jp/contests/abc151/tasks/abc151_c
 
-n, m = gets.split.map(&:to_i)
+_, m = gets.split.map(&:to_i)
 ps_array = Array.new(m) { p, s = gets.split; [p.to_i, s] }
 
 groups = {}
@@ -18,8 +18,11 @@ end
 # puts groups
 
 def process_answers(answers, acs, was)
-  answers.each do |answer|
+  answers.each_with_index do |answer, i|
     if answer == "WA"
+      if i == answer.length - 1
+        return 0, 0
+      end
       was += 1
     elsif answer == "AC"
       acs += 1
