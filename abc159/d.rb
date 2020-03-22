8582ded63bdbf7c3616b9a0af5abd59795b57ac2
Shu Fujita <s.fujita@nard.tech>
2020-03-22 21:42:53 +0900

    [ABC 159 D] Fix file (Ruby, RE)

--- abc159/d.rb
+++ abc159/d.rb
@@ -23,35 +23,24 @@ class Pattern
   end
 end
 
-if a_uniq_size == a.size
-  n.times do
-    puts 0
-  end
-elsif a_uniq_size == 1
-  patterns = (n - 1) * (n - 2) / 2
-  n.times do
-    puts patterns
-  end
-else
-  h = {}
+h = {}
 
-  a.each_with_index do |ai, i|
-    h[ai] ||= Pattern.new(ai)
-    h[ai].increment
-  end
+a.each_with_index do |ai, i|
+  h[ai] ||= Pattern.new(ai)
+  h[ai].increment
+end
 
-  base = h.values.map(&:calc).sum
+base = h.values.map(&:calc).sum
 
-  h2 = {}
+h2 = {}
 
-  a.each_with_index do |ai, i|
-    pattern = h[ai]
-    if h2[ai].nil?
-      result = base - (pattern.count - 1)
-      h2[ai] = result
-      puts result
-    else
-      puts h2[ai]
-    end
+a.each_with_index do |ai, i|
+  pattern = h[ai]
+  if h2[ai].nil?
+    result = base - (pattern.count - 1)
+    h2[ai] = result
+    puts result
+  else
+    puts h2[ai]
   end
 end
