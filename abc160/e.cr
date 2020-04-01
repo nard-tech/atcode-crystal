b0bb67766951472c6d5ae38f7783c74a712039fd
Shu Fujita <s.fujita@nard.tech>
2020-04-02 00:57:44 +0900

    [ABC 160 E] Fix file

--- abc160/e.cr
+++ abc160/e.cr
@@ -6,17 +6,8 @@ p = read_line.split.map(&.to_i64)
 q = read_line.split.map(&.to_i64)
 r = read_line.split.map(&.to_i64)
 
-class Apple
-  def initialize(point : Int64, color : Symbol)
-    @point = point
-    @color = color
-  end
+red_apples = p.sort.reverse[0..(x - 1)]
+green_apples = q.sort.reverse[0..(y - 1)]
+transparent_apples = r.sort.reverse[0..([(x + y), c].min - 1)]
 
-  getter :point, :color
-end
-
-red_apples = p.sort.reverse[0..(x - 1)].map { |i| Apple.new(i, :red) }
-green_apples = q.sort.reverse[0..(y - 1)].map { |i| Apple.new(i, :green) }
-transparent_apples = r.sort.reverse[0..([(x + y), c].min - 1)].map { |i| Apple.new(i, :transparent) }
-
-puts (red_apples + green_apples + transparent_apples).sort_by(&.point).reverse[0..(x + y - 1)].map(&.point).sum
+puts (red_apples + green_apples + transparent_apples).sort.reverse[0..(x + y - 1)].sum
