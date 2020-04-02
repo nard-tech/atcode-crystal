cfc1febfde1f4757fa71c2352f8505e57989300c
Shu Fujita <s.fujita@nard.tech>
2020-04-02 22:00:13 +0900

    [ABC 128 C] Switches - Add file

---
+++ abc128/c.cr
@@ -0,0 +1,33 @@
+# ABC 128 C - Switches
+# https://atcoder.jp/contests/abc128/tasks/abc128_c
+
+n, m = read_line.split.map(&.to_i64)
+ks = Array.new(m) { read_line.split.map(&.to_i64) }
+p_s = read_line.split.map(&.to_i64)
+
+class Lamp
+  def initialize(switch_numbers : Array(Int64), p : Int64)
+    @switch_numbers = switch_numbers
+    @p = p
+  end
+
+  getter :switch_numbers, :p
+
+  def valid?(switch_pattern : String)
+    switch_patterns = switch_numbers.map { |i| switch_pattern[i - 1] }
+    switch_patterns.select { |switch_pattern| switch_pattern == '1' }.size % 2 == p
+  end
+end
+
+lamps = [] of Lamp
+ks.each_with_index do |lamp_ks, i|
+  a = lamp_ks.dup
+  a.shift
+  lamps.push(Lamp.new(a, p_s[i]))
+end
+
+switch_patterns = 1.upto(2 ** n).map { |i| ("0" * n + i.to_s(2))[-n..-1] }
+
+puts switch_patterns.select { |switch_pattern|
+  lamps.all? { |lamp| lamp.valid?(switch_pattern) }
+}.size
