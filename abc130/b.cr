bf9e3d9ba7a02587f299d60ac5850021475ff725
Shu Fujita <s.fujita@nard.tech>
2020-04-04 01:28:03 +0900

    [ABC 130 B] Fix file (Crystal, AC)

--- abc130/b.cr
+++ abc130/b.cr
@@ -2,15 +2,27 @@
 # https://atcoder.jp/contests/abc130/tasks/abc130_b
 
 n, x = read_line.split.map(&.to_i64)
-l = read_line.split.map(&.to_i64)
+l_list = read_line.split.map(&.to_i64)
 
-d_list = [] of Int64
-d = 0_i64
-i = 1_i64
-while d <= x
-  d_list.push(d)
-  d = d + l[i - 1]
-  i += 1_i64
+class Jump
+  def initialize(d : Int64, index : Int64)
+    @d = d
+    @index = index
+  end
+
+  getter :d, :index
 end
 
-puts i - 1_i64
+d_list = [] of Jump
+d_list.push(Jump.new(0_i64, 1_i64))
+
+l_list.each_with_index(1_i64) do |l, i|
+  d_next = d_list.last.d + l
+  if d_next <= x
+    d_list.push(Jump.new(d_next, i + 1))
+  else
+    break
+  end
+end
+
+puts d_list.last.index
