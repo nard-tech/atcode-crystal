a2af3ce21fcf097b7db07c808166ac1124555b7d
Shu Fujita <s.fujita@nard.tech>
2020-04-04 01:33:43 +0900

    [ABC 130 B] Fix file (Ruby, WA)

--- abc130/b.rb
+++ abc130/b.rb
@@ -3,13 +3,13 @@
 
 # ./Main.rb:4: warning: assigned but unused variable - n
 
-_, x = gets.split(/ /).map(&:to_i)
+n, x = gets.split(/ /).map(&:to_i)
 l = gets.split(/ /).map(&:to_i)
 
 d_list = []
 d = 0
 i = 1
-while d <= x
+while d <= x && i < n
   d_list.push(d)
   d = d + l[i - 1]
   i += 1
