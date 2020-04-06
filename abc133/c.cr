fd397230b8f2c8be259f4b62305891dc3e6799c7
Shu Fujita <s.fujita@nard.tech>
2020-04-06 13:21:13 +0900

    [ABC 133 C] Fix file (AC)

--- abc133/c.cr
+++ abc133/c.cr
@@ -4,12 +4,26 @@
 l, r = read_line.split.map(&.to_i64)
 mod = 2019
 
+# puts r - l
+
 if r - l <= mod
-  if l.upto(r).map { |i| i % mod }.includes?(0_i64)
+  mods = l.upto(r).map { |i| i % mod }.to_a
+  # puts mods.first
+
+  if mods.includes?(0_i64)
     puts 0
   else
-    l_mod = l % mod
-    puts l_mod * (l_mod + 1) % mod
+    result = 2018
+
+    0.upto(mods.size - 2) do |i|
+      (i + 1).upto(mods.size - 1) do |j|
+        calc = (mods[i] * mods[j]) % mod
+        # puts calc
+        result = calc if calc < result
+      end
+    end
+
+    puts result
   end
 else
   puts 0
