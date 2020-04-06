fb8dad01b612ae43e3ce1edbfb8fd71017a994ba
Shu Fujita <s.fujita@nard.tech>
2020-04-06 13:17:47 +0900

    [ABC 133 B] Good Distance - Add file (WA)

---
+++ abc133/b.cr
@@ -0,0 +1,28 @@
+# ABC 133 B - Good Distance
+# https://atcoder.jp/contests/abc133/tasks/abc133_b
+
+n, d = read_line.split.map(&.to_i64)
+points = Array.new(n) { read_line.split.map(&.to_i64) }
+
+def distance_square(a : Array(Int64), b : Array(Int64))
+  [a, b].transpose.map { |yz|
+    y, z = yz
+    (y - z) ** 2
+  }.sum
+end
+
+square_numbers = 1.upto(64).map { |i| i ** 2 }.to_a
+
+# puts square_numbers.inspect
+
+count = 0
+
+0.upto(n - 2) do |i|
+  (i + 1).upto(n - 1) do |j|
+    distance_square = distance_square(points[i], points[j])
+    # puts distance_square
+    count += 1 if square_numbers.includes?(distance_square)
+  end
+end
+
+puts count
