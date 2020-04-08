31eefa2c275c936f0644cb4e320e6c8818e8bbfb
Shu Fujita <s.fujita@nard.tech>
2020-04-08 21:27:18 +0900

    [ABC 136 A] Fix file (AC)

--- abc136/a.cr
+++ abc136/a.cr
@@ -2,4 +2,7 @@
 # https://atcoder.jp/contests/abc136/tasks/abc136_a
 
 a, b, c = read_line.split.map(&.to_i64)
-puts [c - (a - b), c].min
+
+rest = a - b
+input = c >= rest ? rest : c
+puts c - input
