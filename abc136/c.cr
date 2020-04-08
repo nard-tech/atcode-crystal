7d76cc790e6281768f33e592cb9cb539a174ff09
Shu Fujita <s.fujita@nard.tech>
2020-04-08 21:31:13 +0900

    [ABC 136 C] Fix file (AC)

--- abc136/c.cr
+++ abc136/c.cr
@@ -11,6 +11,11 @@ class Cell
 
   property :height, :index
   property diff : Int64
+
+  def down
+    @height -= 1 # 現在の cell を 1 小さくする
+    @diff += 1   # 現在の cell 次のセルとの差分を 1 大きくする
+  end
 end
 
 cells = h.map_with_index { |height, i| Cell.new(height, i.to_i64) }
@@ -32,27 +37,25 @@ def valid?(cells : Array(Cell)) : Bool
   return true if diffs.all? { |diff| diff >= 0 }
 
   cells.each_with_index do |cell, i|
-    next if cell.diff >= 0
+    if i == 0   # 最初の cell
+      cell.down #     現在の cell を 1 小さくし，次のセルとの差分を 1 大きくする
+      next
+    end
 
-    # if cell.diff == -1
+    # index が 1 以上
 
-    if i > 0
-      return false if cells[i - 1].diff == 0
+    prev_cell = cells[i - 1]
 
-      cells[i].height -= 1
-      cell.diff = 0_i64
-      cells[i - 1].diff -= 1
-    else
-      cells[i].height -= 1
-      cell.diff = 0_i64
+    if prev_cell.diff == 0            # 前の cell の diff が 0
+      return false if cell.diff == -1 #   現在の cell の diff が -1 なら終わり
+      next                            #   現在の cell の diff が 0 以上なら次の cell へ
+    else                              # 前の cell の diff が 0 より大きい
+      cell.down                       #   現在の cell を 1 小さくし，次のセルとの差分を 1 大きくする
+      prev_cell.diff -= 1             #   前の cell と現在の cell との差分を 1 小さくする
     end
   end
 
   return true
 end
 
-if valid?(cells)
-  puts "Yes"
-else
-  puts "No"
-end
+puts (valid?(cells) ? "Yes" : "No")
