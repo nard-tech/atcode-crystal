51122815314c9aeb2baa819163f4a84f59063a09
Shu Fujita <s.fujita@nard.tech>
2020-04-11 19:46:34 +0900

    [Past 201912 Open C] 3 番目 / The Third

---
+++ past201912open/c.cr
@@ -0,0 +1,5 @@
+# 第一回 アルゴリズム実技検定 過去問 C - 3 番目 / The Third
+# https://atcoder.jp/contests/past201912-open/tasks/past201912_c
+
+integers = read_line.split.map(&.to_i64)
+puts integers.sort.reverse[2]
