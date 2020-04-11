c5452742a2be3e39ee98e56df9f7cec8d5d55c57
Shu Fujita <s.fujita@nard.tech>
2020-04-11 19:58:51 +0900

    [Past 201912 Open E] SNS のログ / Restore (WA)

--- past201912open/e.cr
+++ past201912open/e.cr
@@ -21,53 +21,70 @@ class Query
   property follower_id : Int64
   property followee_id : Int64
 
+  private def follow?(matrix : Array(Array(Int32)), from : Int64, to : Int64)
+    matrix[from][to] == 1
+  end
+
+  private def follow(matrix : Array(Array(Int32)), from : Int64, to : Int64)
+    matrix[from][to] = 1
+    # puts "(#{query_type}) #{from} follows #{to}"
+  end
+
+  private def reverse_follow(matrix : Array(Array(Int32)))
+    # inspect(matrix)
+    queue = [] of Array(Int64)
+
+    matrix.each_with_index do |row, user_id|
+      next if user_id == follower_id
+      next unless follow?(matrix, user_id.to_i64, follower_id.to_i64)
+      queue.push([follower_id.to_i64, user_id.to_i64])
+    end
+
+    process_queue(matrix, queue)
+  end
+
+  private def follow_follow(matrix : Array(Array(Int32)))
+    # inspect(matrix)
+
+    queue = [] of Array(Int64)
+
+    # puts "for follower #{follower_id}"
+    matrix[follower_id].each_with_index do |cell, followee_id|
+      next if followee_id == follower_id
+      next unless follow?(matrix, follower_id.to_i64, followee_id.to_i64)
+
+      # puts "  for #{followee_id}"
+      matrix[followee_id].each_with_index do |cell2, followee_of_followee_id|
+        next if followee_of_followee_id == follower_id
+        next unless follow?(matrix, followee_id.to_i64, followee_of_followee_id.to_i64)
+        queue.push([followee_id.to_i64, followee_of_followee_id.to_i64])
+      end
+    end
+
+    process_queue(matrix, queue)
+  end
+
+  private def process_queue(matrix, queue : Array(Array(Int64)))
+    queue.each do |v|
+      from, to = v
+      follow(matrix, from, to)
+    end
+  end
+
+  private def inspect(matrix : Array(Array(Int32)))
+    matrix.each do |row|
+      puts row.map { |cell| cell == 0 ? "N" : "Y" }.join(" ")
+    end
+  end
+
   def exec(matrix : Array(Array(Int32)))
     # puts "----"
     if @query_type == 1
-      matrix[follower_id][followee_id] = 1
-      # puts "(1) #{follower_id} follows #{followee_id}"
+      follow(matrix, follower_id, followee_id)
     elsif @query_type == 2
-      # matrix.each do |row|
-      #   puts row.map { |cell| cell == 0 ? "N" : "Y" }.join(" ")
-      # end
-
-      queue = [] of Array(Int64 | Int32)
-
-      matrix.each_with_index do |row, user_id|
-        next if user_id == follower_id
-        next if row[follower_id] == 0
-        queue.push([follower_id, user_id])
-        # puts "(2) #{follower_id} follows #{user_id}"
-      end
-
-      queue.each do |v|
-        follower_id, followee_id = v
-        matrix[follower_id][followee_id] = 1
-      end
+      reverse_follow(matrix)
     else
-      # matrix.each do |row|
-      #   puts row.map { |cell| cell == 0 ? "N" : "Y" }.join(" ")
-      # end
-
-      queue = [] of Array(Int64 | Int32)
-
-      # puts "for follower #{follower_id}"
-      matrix[follower_id].each_with_index do |cell, followee_id|
-        next if followee_id == follower_id
-        next if cell == 0
-
-        # puts "  for #{followee_id}"
-        matrix[followee_id].each_with_index do |cell2, followee_of_followee_id|
-          next if cell2 == 0
-          queue.push([follower_id, followee_of_followee_id])
-          # puts "(3) #{follower_id} follows #{followee_of_followee_id} (via #{followee_id})"
-        end
-      end
-
-      queue.each do |v|
-        follower_id, followee_of_followee_id = v
-        matrix[follower_id][followee_of_followee_id] = 1
-      end
+      follow_follow(matrix)
     end
   end
 end
