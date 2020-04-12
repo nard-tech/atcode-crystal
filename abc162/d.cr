4cd6150bf36fa1977be30da2025362d2a68f06a2
Shu Fujita <s.fujita@nard.tech>
2020-04-12 22:58:30 +0900

    [ABC 162 D] Fix file (TLE)

--- abc162/d.cr
+++ abc162/d.cr
@@ -15,14 +15,18 @@ class ColorIndexArray
 
   getter :array
 
-  def select_larger_than(n : Int64)
+  def select_not_larger_than(n : Int64) : Array(Int64)
     return @dict[n].as(Array(Int64)) if !(@dict[n].nil?)
 
-    first_index = @array.bsearch_index { |x, i| x > n }
-    if first_index.nil?
+    first_index_larger_than_n = @array.bsearch_index { |x, i| x > n }
+    if first_index_larger_than_n.nil?
+      a = @array
+      @dict[n] = a
+      a.as(Array(Int64))
+    elsif first_index_larger_than_n == 0
       [] of Int64
     else
-      a = @array[first_index..-1]
+      a = @array[0..(first_index_larger_than_n - 1)]
       @dict[n] = a
       a.as(Array(Int64))
     end
@@ -49,14 +53,38 @@ end
 
 def count_combinations(a1 : ColorIndexArray, a2 : ColorIndexArray, a3 : ColorIndexArray)
   count = 0_i64
-  a1.array.each do |i1|
-    a2_larger_than_i1 = a2.select_larger_than(i1)
-    a2_larger_than_i1.each do |i2|
-      diff_of_i2_and_i1 = i2 - i1
-      a3_larger_than_i2 = a3.select_larger_than(i2).select { |i3| i3 - i2 != diff_of_i2_and_i1 }
-      count += a3_larger_than_i2.size
-    end
+  # a1.array.each do |i1|
+  #   a2_larger_than_i1 = a2.select_larger_than(i1)
+  #   a2_larger_than_i1.each do |i2|
+  #     diff_of_i2_and_i1 = i2 - i1
+  #     a3_larger_than_i2 = a3.select_larger_than(i2).select { |i3| i3 - i2 != diff_of_i2_and_i1 }
+  #     count += a3_larger_than_i2.size
+  #   end
+  # end
+
+  product_of_a2_and_a3 = Array.product(a2.array, a3.array)
+
+  valid_product_of_a2_and_a3 = product_of_a2_and_a3
+    .map { |array| i2, i3 = array; diff = i3 - i2; [i2, i3, diff] }
+    .select { |array| i2, i3, diff = array; diff > 0 }
+
+  # puts valid_product_of_a2_and_a3.inspect
+
+  valid_product_of_a2_and_a3.each do |array|
+    i2, i3, diff = array
+
+    # puts "i2: #{i2}"
+    # puts "a1: #{a1.inspect}"
+    a0 = a1.select_not_larger_than(i2)
+    # puts "a0: #{a0}"
+    a = a0.select { |i1| i2 - i1 != diff }
+
+    # puts a.inspect
+    count += a.size
   end
+
+  # puts ""
+
   count
 end
 
