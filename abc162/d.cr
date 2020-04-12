277e64319a04d45bd2902877192a49ce530fca5f
Shu Fujita <s.fujita@nard.tech>
2020-04-12 22:56:18 +0900

    [ABC 162 D] Fix file (TLE)

--- abc162/d.cr
+++ abc162/d.cr
@@ -19,20 +19,29 @@ def generate_index_array(a : Array(String))
     b_indexes.push(i.to_i64) if str == "B"
   end
 
-  [r_indexes, g_indexes, b_indexes]
+  [r_indexes.sort, g_indexes.sort, b_indexes.sort]
 end
 
 def count_combinations(a1 : Array(Int64), a2 : Array(Int64), a3 : Array(Int64))
   count = 0_i64
   a1.each do |i1|
-    a2_larger_than_i1 = a2.select { |i2| i2 > i1 }
+    a2_larger_than_i1 = select_larger_than(i1, a2)
     a2_larger_than_i1.each do |i2|
       diff_of_i2_and_i1 = i2 - i1
-      a3_larger_than_i2 = a3.select { |i3| i3 > i2 && i3 - i2 != diff_of_i2_and_i1 }
+      a3_larger_than_i2 = select_larger_than(i2, a3).select { |i3| i3 - i2 != diff_of_i2_and_i1 }
       count += a3_larger_than_i2.size
     end
   end
   count
+end
+
+def select_larger_than(n : Int64, array : Array(Int64))
+  first_index = array.bsearch_index { |x, i| x > n }
+  if first_index.nil?
+    [] of Int64
+  else
+    array[first_index..-1]
+  end
 end
 
 r, g, b = generate_index_array(rgb)
