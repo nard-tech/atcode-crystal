86179f8eacfd9f47f33b6c113018198759b194bb
Shu Fujita <s.fujita@nard.tech>
2020-04-12 22:57:55 +0900

    [ABC 162 D] Fix file (TLE)

--- abc162/d.cr
+++ abc162/d.cr
@@ -8,7 +8,28 @@ rgb = s.split(//).map(&.to_s).to_a
 
 # puts rgb
 
-def generate_index_array(a : Array(String))
+class ColorIndexArray
+  def initialize(@array : Array(Int64))
+    @dict = Hash(Int64, Array(Int64) | Nil).new { nil }
+  end
+
+  getter :array
+
+  def select_larger_than(n : Int64)
+    return @dict[n].as(Array(Int64)) if !(@dict[n].nil?)
+
+    first_index = @array.bsearch_index { |x, i| x > n }
+    if first_index.nil?
+      [] of Int64
+    else
+      a = @array[first_index..-1]
+      @dict[n] = a
+      a.as(Array(Int64))
+    end
+  end
+end
+
+def generate_index_array(a : Array(String)) : Array(ColorIndexArray)
   r_indexes = [] of Int64
   g_indexes = [] of Int64
   b_indexes = [] of Int64
@@ -19,44 +40,35 @@ def generate_index_array(a : Array(String))
     b_indexes.push(i.to_i64) if str == "B"
   end
 
-  [r_indexes, g_indexes, b_indexes]
+  [
+    ColorIndexArray.new(r_indexes),
+    ColorIndexArray.new(g_indexes),
+    ColorIndexArray.new(b_indexes),
+  ]
 end
 
-def count_combinations(a1 : Array(Int64), a2 : Array(Int64), a3 : Array(Int64))
+def count_combinations(a1 : ColorIndexArray, a2 : ColorIndexArray, a3 : ColorIndexArray)
   count = 0_i64
-  a1.each do |i1|
-    a2_larger_than_i1 = select_larger_than(i1, a2)
+  a1.array.each do |i1|
+    a2_larger_than_i1 = a2.select_larger_than(i1)
     a2_larger_than_i1.each do |i2|
       diff_of_i2_and_i1 = i2 - i1
-      a3_larger_than_i2 = select_larger_than(i2, a3).select { |i3| i3 - i2 != diff_of_i2_and_i1 }
+      a3_larger_than_i2 = a3.select_larger_than(i2).select { |i3| i3 - i2 != diff_of_i2_and_i1 }
       count += a3_larger_than_i2.size
     end
   end
   count
 end
 
-def select_larger_than(n : Int64, array : Array(Int64))
-  first_index = array.bsearch_index { |x, i| x > n }
-  if first_index.nil?
-    [] of Int64
-  else
-    array[first_index..-1]
-  end
-end
-
 r, g, b = generate_index_array(rgb)
 
-products = [] of Array(Int64)
-products += Array.product(r, g, b)
-products += Array.product(r, b, g)
-products += Array.product(g, r, b)
-products += Array.product(g, b, r)
-products += Array.product(b, r, g)
-products += Array.product(b, g, r)
+sum = 0_i64
 
-# puts products
+sum += count_combinations(r, g, b)
+sum += count_combinations(r, b, g)
+sum += count_combinations(g, r, b)
+sum += count_combinations(g, b, r)
+sum += count_combinations(b, r, g)
+sum += count_combinations(b, g, r)
 
-puts products.select { |array|
-  a, b, c = array
-  a < b < c && b - a != c - b
-}.size
+puts sum
