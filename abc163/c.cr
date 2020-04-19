4b8bec358a21d82efcddf334e2e4bf54b4e0a960
Shu Fujita <s.fujita@nard.tech>
2020-04-19 22:43:55 +0900

    [ABC 163 C] Fix file (AC)

--- abc163/c.cr
+++ abc163/c.cr
@@ -4,23 +4,16 @@
 n = read_line.to_i64
 a = read_line.split.map(&.to_i64)
 
-class Relation
-  def initialize(@boss : Int64, @subordinate : Int64)
-  end
-
-  property :boss, :subordinate
-end
-
-relations = [] of Relation
+relations = Hash(Int64, Int64).new { 0_i64 }
 
 # 社員番号 1
-relations.push(Relation.new(0_i64, 1_i64))
+relations[0_i64] += 1_i64
 
 # 社員番号 2..n
-2.upto(n).each do |i|
-  relations.push(Relation.new(a[i - 2], i.to_i64))
+2_i64.upto(n).each do |i|
+  relations[a[i - 2]] += 1_i64
 end
 
-1.upto(n).each do |i|
-  puts relations.select { |relation| relation.boss == i }.size
+1_i64.upto(n).each do |i|
+  puts relations[i]
 end
