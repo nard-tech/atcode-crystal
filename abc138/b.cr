3275b0dccf10bbd6612c7cdcf269cfe18ee7d32e
Shu Fujita <s.fujita@nard.tech>
2020-04-22 04:31:11 +0900

    [ABC 138 B] Fix file (Crystal, for debug)

--- abc138/b.cr
+++ abc138/b.cr
@@ -4,4 +4,44 @@
 n = read_line.to_i64
 a = read_line.split.map(&.to_i64)
 
-puts 1.0 / a.map { |i| 1.0 / i }.sum
+a_inverse = a.map { |i| 1.0 / i }
+a_inverse_sum = a_inverse.sum
+
+# puts "a"
+# puts a.inspect
+# puts a.class
+# puts ""
+
+# puts "a_inverse"
+# puts a_inverse.inspect
+# puts a_inverse.class
+# puts ""
+
+# puts "a_inverse_sum"
+# puts a_inverse_sum.inspect
+# puts a_inverse_sum.class
+# puts ""
+
+# puts 0.1
+# puts 0.1.class
+# puts ""
+
+# puts "0.1_f64"
+# puts 0.1_f64.class
+# puts ""
+
+# puts 1.0
+# puts 1.0.class
+# puts ""
+
+# puts "1.0_f64"
+# puts 1.0_f64.class
+# puts ""
+
+# puts 1.0_f64 / a_inverse_sum
+puts 1.0 / a_inverse_sum
+# puts 1.0_f64 / 0.1
+# puts 1.0 / 0.1
+# puts a_inverse_sum == 0.1
+# puts a_inverse_sum.class
+# puts 0.1.class
