a37708f41b56acb93411bb647eae1933353d339a
Shu Fujita <s.fujita@nard.tech>
2020-04-24 04:12:11 +0900

    [ABC 139 B] Fix file (AC)

--- abc139/b.cr
+++ abc139/b.cr
@@ -6,13 +6,17 @@ a, b = read_line.split.map(&.to_i64)
 plugs = 0_i64
 taps = 0_i64
 
-while plugs < b
-  if taps == 0
-    plugs += a
-  else
-    plugs += a - 1
+if b == 1
+  puts 0
+else
+  while plugs < b
+    if taps == 0
+      plugs += a
+    else
+      plugs += a - 1
+    end
+    taps += 1
   end
-  taps += 1
-end
 
-puts taps
+  puts taps
+end
