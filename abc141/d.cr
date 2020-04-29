c923d50e5677098c5f234240fd6fe712fc882e7b
Shu Fujita <s.fujita@nard.tech>
2020-04-29 10:05:45 +0900

    [ABC 141 D] Fix file (TLE)

--- abc141/d.cr
+++ abc141/d.cr
@@ -10,29 +10,16 @@ class Item
 
   getter :price
 
-  def discount(tickets : Int64) : Int64
-    [(price * 1.0 * (2_i64**(-tickets.to_f64))).floor, 0].max.to_i64
+  def discount!
+    @price = (price / 2.0).floor.to_i64
   end
 end
 
-items = a.map { |price| Item.new(price) }.sort_by(&.price).reverse # 高い順
-dp = Array.new(n) { Array.new(m + 1) { 0_i64 } }
-
-0_i64.upto(n - 1) do |i|
-  item = items[i]
-
-  0_i64.upto(m) do |j|
-    if i == 0
-      # puts "j: #{j}, item.discount(#{j}) = #{item.discount(j)}" if j < 40
-      dp[i][j] = item.discount(j)
-    elsif j == 0
-      dp[i][j] = dp[i - 1][j] + item.price
-    else
-      candidates = 0_i64.upto(j).map { |k| dp[i - 1][j - k] + item.discount(k) }
-      # puts candidates.inspect
-      dp[i][j] = candidates.min
-    end
-  end
+items = a.map { |price| Item.new(price) }.sort_by(&.price)
+m.times do
+  item = items.last
+  item.discount!
+  items = items.sort_by(&.price)
 end
 
-puts dp[n - 1][m]
+puts items.map(&.price).sum
