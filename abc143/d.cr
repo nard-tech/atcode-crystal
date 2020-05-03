c3542e6f7d3bce44b48a7124b590b07c5bc8b949
Shu Fujita <s.fujita@nard.tech>
2020-05-03 18:09:12 +0900

    [ABC 143 D] Fix file (Crystal, TLE)

--- abc143/d.cr
+++ abc143/d.cr
@@ -2,45 +2,30 @@
 # https://atcoder.jp/contests/abc143/tasks/abc143_d
 
 # TLE
-# https://atcoder.jp/contests/abc143/submissions/12698557
+# https://atcoder.jp/contests/abc143/submissions/12698084
 
 n = read_line.to_i64
-l = read_line.split.map(&.to_i64).sort.reverse
+l = read_line.split.map(&.to_i64).sort
 
-class Stick
-  def initialize(@length : Int64, @index : Int64)
-  end
-
-  getter :length, :index
-
-  def satisfy_triangle_condition?(t)
-    length > t
-  end
-end
-
-sticks = l.map_with_index { |length, i| Stick.new(length, i.to_i64) }
-
-# puts sticks.inspect
+# puts l.inspect
 # puts ""
 
 count = 0
 0_i64.upto(n - 3) do |i|
-  a = sticks[i]
+  a = l[i]
   (i + 1).upto(n - 2) do |j|
-    b = sticks[j]
-    c_candidates = sticks[(j + 1)..(n - 1)]
-    t = a.length - b.length
-    c_over = c_candidates.bsearch { |stick| !stick.satisfy_triangle_condition?(t) }
-    if c_over.nil?
-      c_last_index = n - 1
-    else
-      c_last_index = c_over.as(Stick).index - 1
-    end
+    b = l[j]
+    c_candidates = l[(j + 1)..(n - 1)]
+    t = a + b
     # puts "--------"
     # puts c_candidates.inspect
-    # puts "a: #{a}, b: #{b}, a - b: #{t} -> #{c_last_index - j}"
-    # puts ""
-    count += c_last_index - j
+    # puts "a: #{a}, b: #{b}, a + b: #{t}"
+    c_over_index = c_candidates.bsearch_index { |c| c >= t }
+    if c_over_index.nil?
+      count += (n - 1) - j
+    else
+      count += c_over_index
+    end
   end
 end
 
