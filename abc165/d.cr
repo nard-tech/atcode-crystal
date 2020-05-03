bfe9faa633064c09ade3a5dc07c47860caae74cf
Shu Fujita <s.fujita@nard.tech>
2020-05-03 15:24:06 +0900

    [ABC 165 D] Fix file (Crystal, WA)

--- abc165/d.cr
+++ abc165/d.cr
@@ -5,7 +5,7 @@ a, b, n = read_line.split.map(&.to_i64)
 if n < b
   x = n
   x_per_b = x * 1.0.to_f64 / b
-  puts ((a * x_per_b).floor - a * x_per_b.floor).to_i64
+  puts (a * x_per_b).floor.to_i64 - a * x_per_b.floor.to_i64
 else
   puts a - 1
 end
