c8eb0b7e5013ea10941b792c26e103b80ad4f7c3
Shu Fujita <s.fujita@nard.tech>
2020-05-03 15:56:15 +0900

    [ABC 165 D] Fix file (Crystal, AC)

--- abc165/d.cr
+++ abc165/d.cr
@@ -2,15 +2,16 @@
 # https://atcoder.jp/contests/abc165/tasks/abc165_d
 
 a, b, n = read_line.split.map(&.to_i64)
-if n < b
-  x = n
+
+def calc(a, b, x)
   x_per_b = x * 1.0.to_f64 / b
-  puts (a * x_per_b).floor.to_i64 - a * x_per_b.floor.to_i64
+  (a * x_per_b).floor.to_i64 - a * x_per_b.floor.to_i64
+end
+
+if n < b
+  puts calc(a, b, n)
 elsif a > b
-  puts (0..(b - 1)).max_of { |x|
-    x_per_b = x * 1.0.to_f64 / b
-    (a * x_per_b).floor.to_i64 - a * x_per_b.floor.to_i64
-  }
+  puts (0..(b - 1)).max_of { |x| calc(a, b, x) }
 else
   puts a - 1
 end
