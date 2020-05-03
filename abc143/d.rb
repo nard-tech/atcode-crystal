66fddf661de45ae07eb4f5a6cb623118b4d743a0
Shu Fujita <s.fujita@nard.tech>
2020-05-03 16:34:55 +0900

    [ABC 143 D] Fix file (Ruby, AC)

--- abc143/d.rb
+++ abc143/d.rb
@@ -1,31 +1,44 @@
 # ABC 143 D - Triangles
 # https://atcoder.jp/contests/abc143/tasks/abc143_d
 
-# 1629 ms, 1916 KB
-# https://atcoder.jp/contests/abc143/submissions/12697616
+# 1968 ms, 2168 KB
+# https://atcoder.jp/contests/abc143/submissions/12697681
 
 n = gets.chomp.to_i
-l = gets.chomp.split(/ /).map(&:to_i).sort.reverse
+l = gets.chomp.split(/ /).map(&:to_i).sort
+
+class Stick
+  def initialize(length, index)
+    @length = length
+    @index = index
+  end
+
+  attr_reader :length, :index
+end
+
+sticks = l.map.with_index(0) { |length, i| Stick.new(length, i) }
 
 # puts sticks.inspect
 # puts ""
 
 count = 0
 0.upto(n - 3) do |i|
-  a = l[i]
+  a = sticks[i]
   (i + 1).upto(n - 2) do |j|
-    b = l[j]
-    c_candidates = l[(j + 1)..(n - 1)]
-    t = a - b
+    b = sticks[j]
+    c_candidates = sticks[(j + 1)..(n - 1)]
+    t = a.length + b.length
+    c_over = c_candidates.bsearch { |stick| stick.length >= t }
+    if c_over.nil?
+      c_last_index = n - 1
+    else
+      c_last_index = c_over.index - 1
+    end
     # puts "--------"
     # puts c_candidates.inspect
-    # puts "a: #{a}, b: #{b}, a - b: #{t}"
-    c_over_index = c_candidates.bsearch_index { |c| c <= t }
-    if c_over_index.nil?
-      count += (n - 1) - j
-    else
-      count += c_over_index
-    end
+    # puts "a: #{a}, b: #{b}, a + b: #{t} -> #{c_last_index - j}"
+    # puts ""
+    count += c_last_index - j
   end
 end
 
