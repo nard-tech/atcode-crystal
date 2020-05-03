3787d0935065298ab4f469cfaa25ff9148c555de
Shu Fujita <s.fujita@nard.tech>
2020-05-03 15:26:16 +0900

    [ABC 165 D] Fix file (Ruby, WA)

--- abc165/d.rb
+++ abc165/d.rb
@@ -1,11 +1,13 @@
 # ABC 165 D - Floor Function
 # https://atcoder.jp/contests/abc165/tasks/abc165_d
 
+require 'bigdecimal'
+
 a, b, n = gets.split(/ /).map(&:to_i)
 if n < b
   x = n
-  x_per_b = x * 1.0 / b
-  puts (a * x_per_b).floor - a * x_per_b.floor
+  x_per_b = BigDecimal(x.to_s) * 1.0 / BigDecimal(b.to_s)
+  puts (a * BigDecimal(x_per_b.to_s)).floor - a * BigDecimal(x_per_b.to_s).floor
 else
   puts a - 1
 end
