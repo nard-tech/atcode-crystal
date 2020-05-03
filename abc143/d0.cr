42ab096b1974bffcd09424326938d180df0be3f4
Shu Fujita <s.fujita@nard.tech>
2020-05-03 17:32:05 +0900

    [ABC 143 D] Fix file (Crystal, TLE)

--- abc143/d0.cr
+++ abc143/d0.cr
@@ -2,15 +2,22 @@
 # https://atcoder.jp/contests/abc143/tasks/abc143_d
 
 # TLE
-# https://atcoder.jp/contests/abc143/submissions/12697895
+# https://atcoder.jp/contests/abc143/submissions/12697944
 
 n = read_line.to_i64
-l = read_line.split.map(&.to_i64)
+l = read_line.split.map(&.to_i64).sort.reverse
+
+h = Hash(Int64, Int64).new
+l.group_by(&.itself).each do |k, v|
+  h[k] = v.size.to_i64
+end
+
+l_uniq = l.uniq
 
 count = 0
-l.combinations(3).each do |sticks|
+l.combinations(3).to_a.uniq.each do |sticks|
   a, b, c = sticks
   base = (a + b + c) * 0.5
-  count += 1 if [a, b, c].all? { |i| i < base }
+  count += h[a] * h[b] * h[c] if [a, b, c].all? { |i| i < base }
 end
 puts count
