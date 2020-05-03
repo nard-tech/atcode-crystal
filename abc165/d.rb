4ea69c71374e78845ff86ec8481139b33ab1b4ac
Shu Fujita <s.fujita@nard.tech>
2020-05-03 15:26:28 +0900

    [ABC 165 D] Fix file (Ruby, WA)

--- abc165/d.rb
+++ abc165/d.rb
@@ -6,8 +6,8 @@ require 'bigdecimal'
 a, b, n = gets.split(/ /).map(&:to_i)
 if n < b
   x = n
-  x_per_b = BigDecimal(x.to_s) * 1.0 / BigDecimal(b.to_s)
-  puts (a * BigDecimal(x_per_b.to_s)).floor - a * BigDecimal(x_per_b.to_s).floor
+  x_per_b = BigDecimal(x.to_s) * BigDecimal(1.0.to_s) / BigDecimal(b.to_s)
+  puts (BigDecimal(a) * BigDecimal(x_per_b.to_s)).floor - BigDecimal(a) * BigDecimal(x_per_b.to_s).floor
 else
   puts a - 1
 end
