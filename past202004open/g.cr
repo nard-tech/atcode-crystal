23771727bf4a5128294a6fef6fd65568eb8ed753
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:33:31 +0900

    [Past 202004 Open G] Fix file (Crystal, TLE)

--- past202004open/g.cr
+++ past202004open/g.cr
@@ -2,14 +2,14 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_g
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12815961
+# https://atcoder.jp/contests/past202004-open/submissions/12819893
 
 q = read_line.to_i64
 queries = Array.new(q) { read_line.split }
 
 s = ""
 
-def calc_deleted_chars_index(deleted_chars : String) : Int64
+def calc_deleted_chars_index(deleted_chars : String, dict : Hash(Int64, Int64)) : Int64
   n = 0_i64
   a = deleted_chars.split(//)
   # puts a.inspect
@@ -18,10 +18,13 @@ def calc_deleted_chars_index(deleted_chars : String) : Int64
   h.each do |k, v|
     next if k == ""
     i = v.size.to_i64
-    n += i ** 2
+    dict[i] = i ** 2 unless dict.has_key?(i)
+    n += dict[i]
   end
   n
 end
+
+h = Hash(Int64, Int64).new
 
 queries.each do |query|
   if query.first == "1"
@@ -42,6 +45,6 @@ queries.each do |query|
       s = s[d..-1]
     end
 
-    puts calc_deleted_chars_index(deleted_chars)
+    puts calc_deleted_chars_index(deleted_chars, h)
   end
 end
