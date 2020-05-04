86d9a7b9e031e3e3d5d4630ae42caa9394b83d3d
Shu Fujita <s.fujita@nard.tech>
2020-05-04 14:07:32 +0900

    [ABC 166 D] Fix file (Ruby, AC)

--- abc166/d.rb
+++ abc166/d.rb
@@ -2,21 +2,19 @@
 # https://atcoder.jp/contests/abc166/tasks/abc166_d
 
 # AC
-# https://atcoder.jp/contests/abc166/submissions/12757667
-
-require 'prime'
+# https://atcoder.jp/contests/abc166/submissions/12807379
 
 x = gets.chomp.to_i
 
 h = {}
-(0..999).each do |i|
+(0..120).each do |i|
   h[i] = i ** 5
   h[-i] = - h[i]
 end
 
 def search(x, h)
-  (-999..999).to_a.each do |a|
-    (-999..999).to_a.each do |b|
+  (-120..120).to_a.each do |a|
+    (-120..120).to_a.each do |b|
       return [a, b] if h[a] - h[b] == x
     end
   end
