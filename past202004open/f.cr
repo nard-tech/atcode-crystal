4aaa1b81897aa7677c3d134c9fca2a0110c39a3f
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:28:35 +0900

    [Past 202004 Open F] Fix file (Crystal, TLE)

--- past202004open/f.cr
+++ past202004open/f.cr
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12818399
+# https://atcoder.jp/contests/past202004-open/submissions/12818746
 
 n = read_line.to_i64
 ab_s = Array.new(n) { read_line.split.map(&.to_i64) }
@@ -14,26 +14,33 @@ class Task
   getter :begin_at_or_after, :point
 end
 
-tasks = ab_s.map { |ab| a, b = ab; Task.new(a, b) }.sort_by(&.begin_at_or_after)
+tasks = ab_s.map { |ab| a, b = ab; Task.new(a, b) }.sort_by(&.point).reverse.group_by(&.begin_at_or_after)
 
-def serach_task(tasks : Array(Task), day : Int64, n : Int64) : Task
-  over_index = tasks.bsearch_index { |task| task.begin_at_or_after > day }
-  over_index = n if over_index.nil?
-  task = tasks[0..(over_index - 1)].max_by(&.point)
-  tasks.delete(task)
+# puts tasks.inspect
+
+def serach_task(tasks : Hash(Int64, Array(Task)), day : Int64, n : Int64) : Task
+  candidates = [] of Task
+  (1_i64..day).each do |i|
+    next unless tasks.has_key?(i)
+    next if tasks[i].empty?
+    candidates.push(tasks[i].first)
+  end
+  task = candidates.max_by(&.point)
+  tasks[task.begin_at_or_after].delete(task)
   task
 end
 
 results = [] of Int64
 task_points = Hash(Int64, Int64).new { 0_i64 }
 
-1.upto(n) do |k|
-  task = serach_task(tasks, k.to_i64, n)
+1_i64.upto(n) do |k|
+  task = serach_task(tasks, k, n)
   if k == 1
-    task_points[1_i64] = task.point
+    point = task.point
   else
-    task_points[k.to_i64] = task_points[k - 1] + task.point
+    point = task_points[k - 1] + task.point
   end
+  task_points[k] = point
 end
 
 task_points.values.each do |v|
