d2fb0fa8f38c2afd57a3af25185b8062c17e7bc7
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:30:28 +0900

    [Past 202004 Open F] Fix file (Ruby, TLE)

--- past202004open/f.rb
+++ past202004open/f.rb
@@ -2,30 +2,41 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12818997
+# https://atcoder.jp/contests/past202004-open/submissions/12819608
 
 n = gets.chomp.to_i
 ab_s = Array.new(n) { gets.chomp.split.map(&:to_i) }
 
-tasks = ab_s.sort_by(&:last).reverse
-schedule = (0..(n - 1)).to_a
+class Task
+  def initialize(begin_at_or_after, point)
+    @begin_at_or_after = begin_at_or_after
+    @point = point
+  end
+
+  attr_reader :begin_at_or_after, :point
+end
+
+tasks = ab_s.map { |a, b| Task.new(a, b) }.sort_by(&:point).reverse
+schedule = Array.new(n) { nil }
 
 tasks.each do |task|
-  day_index = task.first
-  if schedule[day_index - 1].instance_of?(Array)
-    i = schedule.find { |task_of_day| task_of_day.is_a?(Integer) && task_of_day >= task.first - 1 }
-    schedule[i] = task
+  if schedule[task.begin_at_or_after - 1].nil?
+    schedule[task.begin_at_or_after - 1] = task
   else
-    schedule[day_index - 1] = task
+    day_index = task.begin_at_or_after - 1
+    while !schedule[day_index].nil?
+      day_index += 1
+    end
+    schedule[day_index] = task
   end
 end
 
 results = []
 schedule.each do |task|
   if results.empty?
-    results << task.last
+    results.push(task.point)
   else
-    results << results.last + task.last
+    results.push(results.last + task.point)
   end
 end
 
