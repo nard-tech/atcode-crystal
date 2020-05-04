533ab0157b4aaa98a310ca3bb5c20521098aeb59
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:34:18 +0900

    [Past 202004 Open G] Fix file (Crystal, TLE)

--- past202004open/g.cr
+++ past202004open/g.cr
@@ -2,15 +2,16 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_g
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12819893
+# https://atcoder.jp/contests/past202004-open/submissions/12820388
 
 q = read_line.to_i64
 queries = Array.new(q) { read_line.split }
 
 s = ""
 
-def calc_deleted_chars_index(deleted_chars : String, dict : Hash(Int64, Int64)) : Int64
+def calc_deleted_chars_index(deleted_chars : String, square_dict : Hash(Int64, Int64), chars_dict : Hash(String, Int64)) : Int64
   n = 0_i64
+  return chars_dict[deleted_chars] if chars_dict.has_key?(deleted_chars)
   a = deleted_chars.split(//)
   # puts a.inspect
   h = a.group_by(&.itself)
@@ -18,13 +19,15 @@ def calc_deleted_chars_index(deleted_chars : String, dict : Hash(Int64, Int64)) 
   h.each do |k, v|
     next if k == ""
     i = v.size.to_i64
-    dict[i] = i ** 2 unless dict.has_key?(i)
-    n += dict[i]
+    square_dict[i] = i ** 2 unless square_dict.has_key?(i)
+    n += square_dict[i]
   end
+  chars_dict[deleted_chars] = n
   n
 end
 
-h = Hash(Int64, Int64).new
+square_dict = Hash(Int64, Int64).new
+chars_dict = Hash(String, Int64).new
 
 queries.each do |query|
   if query.first == "1"
@@ -45,6 +48,6 @@ queries.each do |query|
       s = s[d..-1]
     end
 
-    puts calc_deleted_chars_index(deleted_chars, h)
+    puts calc_deleted_chars_index(deleted_chars, square_dict, chars_dict)
   end
 end
