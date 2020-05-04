14b5c8e4ea4b7897fb9cdee57d640abcd8dd40b7
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:37:16 +0900

    [Past 202004 Open F] Fix file (Ruby, TLE)

--- past202004open/f.rb
+++ past202004open/f.rb
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12821343
+# https://atcoder.jp/contests/past202004-open/submissions/12821553
 
 n = gets.chomp.to_i
 ab_s = Array.new(n) { gets.chomp.split.map(&:to_i) }
@@ -11,8 +11,13 @@ tasks = ab_s.sort_by(&:last).reverse
 schedule = (0..(n - 1)).to_a
 
 tasks.each do |task|
-  day_index = schedule.find { |task_of_day| task_of_day.is_a?(Integer) && task_of_day >= task.first - 1 }
-  schedule[day_index] = task
+  day_index = task.first
+  if schedule[day_index - 1].instance_of?(Array)
+    i = schedule.find { |task_of_day| task_of_day.is_a?(Integer) && task_of_day >= task.first - 1 }
+    schedule[i] = task
+  else
+    schedule[day_index - 1] = task
+  end
 end
 
 results = []
