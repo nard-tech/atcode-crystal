2c6bdfa62c5fdf0566f109ee9fa3b4668c07ae52
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:25:19 +0900

    [Past 202004 Open F] Fix file (Crystal, TLE)

--- past202004open/f.cr
+++ past202004open/f.cr
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12814533
+# https://atcoder.jp/contests/past202004-open/submissions/12814931
 
 n = read_line.to_i64
 ab_s = Array.new(n) { read_line.split.map(&.to_i64) }
@@ -17,19 +17,18 @@ end
 tasks = ab_s.map { |ab| a, b = ab; Task.new(a, b) }.sort_by(&.point).reverse
 
 results = [] of Int64
+tasks_to_execute = Hash(Int64, Array(Task)).new { [] of Task }
 
 1.upto(n) do |k|
-  tasks_to_execute = [] of Task
-  tasks_copy = tasks.dup
-  1.upto(k) do |j|
-    task_to_execute = tasks_copy.find { |task| task.begin_at_or_after <= j }
-    raise "Nil is not valid for k: #{k}, day #{j}" if task_to_execute.nil?
-    tasks_copy.delete(task_to_execute)
-    tasks_to_execute.push(task_to_execute)
+  task = tasks.find { |task| task.begin_at_or_after <= k }.as(Task)
+  tasks.delete(task)
+  if k == 1
+    tasks_to_execute[1_i64] = [task]
+  else
+    tasks_to_execute[k.to_i64] = tasks_to_execute[k - 1] + [task]
   end
-  results.push(tasks_to_execute.sum(&.point))
 end
 
-results.each do |result|
-  puts result
+tasks_to_execute.values.each do |v|
+  puts v.sum(&.point)
 end
