b978a4e79bd592ad54542d01c1bedfb22be29449
Shu Fujita <s.fujita@nard.tech>
2020-05-04 14:00:18 +0900

    [ABC 166 A] A?C - Add file (AC)

---
+++ abc166/a.cr
@@ -0,0 +1,12 @@
+# ABC 166 A - A?C
+# https://atcoder.jp/contests/abc166/tasks/abc166_a
+
+# AC
+# https://atcoder.jp/contests/abc166/submissions/12706127
+
+s = read_line
+if s == "ABC"
+  puts "ARC"
+else
+  puts "ABC"
+end
