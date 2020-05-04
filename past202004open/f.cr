3b59f0a7862043851c6cc2120ae4cbe5cd6e2756
Shu Fujita <s.fujita@nard.tech>
2020-05-04 21:28:01 +0900

    [Past 202004 Open F] Fix file (Crystal, TLE)

--- past202004open/f.cr
+++ past202004open/f.cr
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12815301
+# https://atcoder.jp/contests/past202004-open/submissions/12818399
 
 n = read_line.to_i64
 ab_s = Array.new(n) { read_line.split.map(&.to_i64) }
@@ -25,17 +25,17 @@ def serach_task(tasks : Array(Task), day : Int64, n : Int64) : Task
 end
 
 results = [] of Int64
-tasks_to_execute = Hash(Int64, Array(Task)).new { [] of Task }
+task_points = Hash(Int64, Int64).new { 0_i64 }
 
 1.upto(n) do |k|
   task = serach_task(tasks, k.to_i64, n)
   if k == 1
-    tasks_to_execute[1_i64] = [task]
+    task_points[1_i64] = task.point
   else
-    tasks_to_execute[k.to_i64] = tasks_to_execute[k - 1] + [task]
+    task_points[k.to_i64] = task_points[k - 1] + task.point
   end
 end
 
-tasks_to_execute.values.each do |v|
-  puts v.sum(&.point)
+task_points.values.each do |v|
+  puts v
 end
