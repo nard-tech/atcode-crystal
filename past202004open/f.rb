3417cf594c7160f700b85ad13fb2216a9479a2f7
Shu Fujita <s.fujita@nard.tech>
2020-05-04 22:11:03 +0900

    [Past 202004 Open F] Fix file (Ruby, AC)

--- past202004open/f.rb
+++ past202004open/f.rb
@@ -2,33 +2,39 @@
 # https://atcoder.jp/contests/past202004-open/tasks/past202004_f
 
 # TLE
-# https://atcoder.jp/contests/past202004-open/submissions/12821553
+# https://atcoder.jp/contests/past202004-open/submissions/12826432
 
 n = gets.chomp.to_i
 ab_s = Array.new(n) { gets.chomp.split.map(&:to_i) }
 
-tasks = ab_s.sort_by(&:last).reverse
-schedule = (0..(n - 1)).to_a
-
-tasks.each do |task|
-  day_index = task.first
-  if schedule[day_index - 1].instance_of?(Array)
-    i = schedule.find { |task_of_day| task_of_day.is_a?(Integer) && task_of_day >= task.first - 1 }
-    schedule[i] = task
-  else
-    schedule[day_index - 1] = task
+class Task
+  def initialize(begin_at_or_after, point)
+    @begin_at_or_after = begin_at_or_after
+    @point = point
   end
+
+  attr_reader :begin_at_or_after, :point
 end
 
-results = []
-schedule.each do |task|
-  if results.empty?
-    results << task.last
-  else
-    results << results.last + task.last
-  end
-end
+tasks = ab_s.map { |a, b| Task.new(a, b) }.sort_by(&:point).reverse.group_by(&:begin_at_or_after)
 
-results.each do |result|
-  puts result
+v = Array.new(101, 0)
+sum_of_point = 0
+
+(1..n).each do |i|
+  if tasks[i]
+    tasks[i].each do |task|
+      v[task.point] += 1
+    end
+  end
+
+  (1..100).reverse_each do |point|
+    if v[point] > 0
+      v[point] -= 1
+      sum_of_point += point
+      break
+    end
+  end
+
+  puts sum_of_point
 end
