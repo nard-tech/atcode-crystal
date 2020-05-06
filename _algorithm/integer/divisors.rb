2991c0452a99ae4a1cf595c6157e17c200960093
Shu Fujita <s.fujita@nard.tech>
2020-05-06 21:10:35 +0900

    Add _algorithm/integer/divisors.rb

---
+++ _algorithm/integer/divisors.rb
@@ -0,0 +1,18 @@
+# 自然数の約数を配列として返すメソッド
+# @param n [Integer] 自然数
+# @return [Array<Integer>] 約数（自然数）の配列（昇順でソート済み）
+def divisors(n)
+  list_a = []
+  list_b = []
+
+  (1..(Math.sqrt(n))).each do |i|
+    div, mod = n.divmod(i)
+    if mod == 0
+      list_a.push(i)
+      list_b.push(div)
+    end
+  end
+
+  list_b.pop if list_b.last == list_a.last
+  list_a + list_b.reverse
+end
