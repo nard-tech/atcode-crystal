e45dcba6fcc9b6f4d30997a603f932276eaddae0
Shu Fujita <s.fujita@nard.tech>
2020-05-06 20:57:33 +0900

    [ABC 144 C] Walk on Multiplication Table - Add file (Ruby, TLE)

---
+++ abc144/c.rb
@@ -0,0 +1,45 @@
+# ABC 144 C - Walk on Multiplication Table
+# https://atcoder.jp/contests/abc144/tasks/abc144_c
+
+# TLE
+# https://atcoder.jp/contests/abc144/submissions/12895361
+
+require 'prime'
+
+n = gets.chomp.to_i
+
+def divisors(x)
+  list = [1]
+  primes = []
+
+  Prime.prime_division(x).each do |prime|
+    prime[1].times do
+      primes << prime[0]
+    end
+  end
+
+  1.upto(primes.size) do |i|
+    primes.combination(i) do |prime|
+      list << prime.inject(:*)
+    end
+  end
+
+  list.uniq!
+  list.sort!
+  return list
+end
+
+result = 10**12 + 1
+divisors = divisors(n)
+
+# puts divisors
+
+divisors.each do |divisor|
+  another_divisor = n / divisor
+  i = divisor + another_divisor
+  if result > i
+    result = i
+  end
+end
+
+puts result - 2
