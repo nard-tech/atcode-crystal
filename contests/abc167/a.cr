0edd815c75d36ab27d6f050b60afe8e917515db3
Shu Fujita <s.fujita@nard.tech>
2020-05-10 22:30:06 +0900

    [ABC 167 A] Registration - Add file (AC)

---
+++ contests/abc167/a.cr
@@ -0,0 +1,17 @@
+# ABC 167 A - Registration
+# https://atcoder.jp/contests/contests/abc167/tasks/contests/abc167_a
+
+# AC
+# https://atcoder.jp/contests/abc167/submissions/13022321
+
+s = read_line
+t = read_line
+
+s_size = s.size
+t_size = t.size
+
+if t_size == s_size + 1 && t[0..-2] == s
+  puts "Yes"
+else
+  puts "No"
+end
