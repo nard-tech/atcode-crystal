aa425a71df8126887545bb70a47252b2af11bf76
Shu Fujita <s.fujita@nard.tech>
2020-05-10 22:41:40 +0900

    [ABC 167 B] Fix file (WA)

--- contests/abc167/b.cr
+++ contests/abc167/b.cr
@@ -1,13 +1,13 @@
-# ABC 167 B - Easy Linear Programming
+# ABC 167 B
 # https://atcoder.jp/contests/contests/abc167/tasks/contests/abc167_b
 
 # WA
-# https://atcoder.jp/contests/abc167/submissions/13077366
+# https://atcoder.jp/contests/abc167/submissions/13078687
 
 a, b, c, k = read_line.split.map(&.to_i64)
 
 if a >= k
-  puts a
+  puts k
 elsif a + b >= k
   puts a
 else
