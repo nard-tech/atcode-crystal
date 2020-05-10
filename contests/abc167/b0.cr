e6f49ae5bac15c8377af0b8824abd6c56e156577
Shu Fujita <s.fujita@nard.tech>
2020-05-10 22:43:04 +0900

    [ABC 167 B] Easy Linear Programming - Add file (AC)

---
+++ contests/abc167/b0.cr
@@ -0,0 +1,31 @@
+# ABC 167 B
+# https://atcoder.jp/contests/contests/abc167/tasks/contests/abc167_b
+
+# AC
+# https://atcoder.jp/contests/abc167/submissions/13049285
+
+a, b, c, k = read_line.split.map(&.to_i64)
+
+i = 0
+
+na = [a, k].min
+i += na
+k -= na
+
+if k == 0
+  puts i
+  exit
+end
+
+nb = [b, k].min
+k -= nb
+
+if k == 0
+  puts i
+  exit
+end
+
+nc = [c, k].min
+i -= nc
+
+puts i
