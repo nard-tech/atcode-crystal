e18f890ef515077ecac13317dbc55da5f5d4ceff
Shu Fujita <s.fujita@nard.tech>
2020-05-10 22:39:28 +0900

    [ABC 167 B] Fix file (WA)

--- contests/abc167/b.cr
+++ contests/abc167/b.cr
@@ -2,12 +2,14 @@
 # https://atcoder.jp/contests/contests/abc167/tasks/contests/abc167_b
 
 # WA
-# https://atcoder.jp/contests/abc167/submissions/13026864
+# https://atcoder.jp/contests/abc167/submissions/13045916
 
 a, b, c, k = read_line.split.map(&.to_i64)
 
-if a + b <= k
-  puts a + b - c
-else
+if a > k
+  puts k
+elsif a + b > k
   puts a
+else
+  puts a + b - c
 end
