3e74e0f896ab8f0a9b0d6be4ab451ee62fb6efc3
Shu Fujita <s.fujita@nard.tech>
2020-05-16 21:56:42 +0900

    Fix script for cumulative calculation (Crystal)

--- lib/cumulative_sum.cr
+++ lib/cumulative_sum.cr
@@ -1,38 +1,65 @@
-macro define_cumulative_calculation_class(name, begin_with, operator)
-  class {{name}}(T)
-    def self.calc(array : Array(T)) : Array(T)
-      new(array).calc
+macro define_cumulative_calculation_class(class_name, first_element, operator_for_calc, operator_for_between)
+  class {{class_name}}(T)
+    def self.generate(array : Array(T), &block)
+      new(array) do |last, i|
+        yield(last, i)
+      end
     end
 
-    def self.calc(array : Array(T), &block) : Array(T)
-      new(array).calc do |last, i|
+    def self.generate(array : Array(T))
+      new(array)
+    end
+
+    def initialize(@array : Array(T), &block)
+      @result = calc(@array) do |last, i|
         yield(last, i)
       end
     end
 
     def initialize(@array : Array(T))
+      @result = calc(@array)
     end
 
     getter :array
+    getter result : Array(T)
 
-    def calc(&block) : Array(T)
+    def between(i : Int32 | Int64, j : Int32 | Int64, &block)
+      raise "ArgumentError: The second argument should be larger than the first argument." unless i <= j
+      j_th = @result[j + 1]
+      i_th = @result[i]
+      yield(i_th, j_th)
+    end
+
+    def between(i : Int32 | Int64, j : Int32 | Int64)
+      between(i, j) do |i_th, j_th|
+        {% if operator_for_between == :- %}
+          j_th - i_th
+        {% elsif operator_for_between == :// %}
+          j_th // i_th
+        {% else %}
+          raise "ArgumentError: operator is not valid."
+        {% end %}
+      end
+    end
+
+    private def calc(a : Array(T), &block) : Array(T)
       result = [] of T
-      result.push({{begin_with}})
+      result.push(T.new({{first_element}}))
 
-      array.each do |i|
+      a.each do |i|
         result.push(yield(result.last, i))
       end
 
       result
     end
 
-    def calc : Array(T)
-      {% if operator == :+ %}
-        calc do |last, i|
+    private def calc(a : Array(T)) : Array(T)
+      {% if operator_for_calc == :+ %}
+        calc(a) do |last, i|
           last + i
         end
-      {% elsif operator == :* %}
-        calc do |last, i|
+      {% elsif operator_for_calc == :* %}
+        calc(a) do |last, i|
           last * i
         end
       {% else %}
@@ -42,5 +69,5 @@ macro define_cumulative_calculation_class(name, begin_with, operator)
   end
 end
 
-define_cumulative_calculation_class(CumulativeSum, 0, :+)
-define_cumulative_calculation_class(CumulativeProduct, 1, :*)
+define_cumulative_calculation_class(CumulativeSum, 0, :+, :-)
+define_cumulative_calculation_class(CumulativeProduct, 1, :*, ://)
