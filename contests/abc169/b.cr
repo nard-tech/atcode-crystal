84c701e83c5b810171fb4a20360e9d75f925df8d
Shu Fujita <s.fujita@nard.tech>
2020-05-31 22:25:24 +0900

    [ABC 169 B] Fix file (Crystal, AC)

--- contests/abc169/b.cr
+++ contests/abc169/b.cr
@@ -6,17 +6,25 @@
 n = read_line.to_i64
 a = read_line.split.map(&.to_i64)
 
-i = 1_i64
 if a.any?(&.zero?)
   puts 0
-else
-  a.each do |element|
+  exit
+end
+
+i = 1_i64
+
+a.each do |element|
+  begin
     i *= element
-    if i > 10_i64 ** 18
-      i = -1_i64
-      break
-    end
+  rescue e : OverflowError
+    i = -1_i64
+    break
   end
 
-  puts i
+  if i > 10_i64 ** 18
+    i = -1_i64
+    break
+  end
 end
+
+puts i
