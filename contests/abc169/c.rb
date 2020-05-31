250903c72f351b65913e339c1875799e8baafd04
Shu Fujita <s.fujita@nard.tech>
2020-05-31 21:58:00 +0900

    [ABC 169 C] Multiplication 3 - Add file (AC, Ruby)

--- contests/abc169/c.rb
+++ contests/abc169/c.rb
@@ -1,10 +1,12 @@
 # ABC 169 C - Multiplication 3
 # https://atcoder.jp/contests/abc169/tasks/abc169_c
 
-# https://atcoder.jp/contests/abc169/submissions/13796542
+# https://atcoder.jp/contests/abc169/submissions/13800503
+
+require 'bigdecimal'
 
 a_as_str, b_as_str = gets.chomp.split(/ /)
 a = a_as_str.to_i
-b = b_as_str.to_f
+b = BigDecimal(b_as_str)
 
-puts (a * (b * 100).to_i) / 100
+puts a * (b * 100).to_i / 100
