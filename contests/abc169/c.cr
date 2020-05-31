a8f1e1a3e69940a2b6f2decfe3b4de3a86a62112
Shu Fujita <s.fujita@nard.tech>
2020-05-31 22:11:46 +0900

    [ABC 169 C] Fix file (AC, Crystal)

--- contests/abc169/c.cr
+++ contests/abc169/c.cr
@@ -1,10 +1,12 @@
 # ABC 169 C - Multiplication 3
 # https://atcoder.jp/contests/abc169/tasks/abc169_c
 
-# https://atcoder.jp/contests/abc169/submissions/13790616
+# https://atcoder.jp/contests/abc169/submissions/13862181
+
+require "big"
 
 a_as_str, b_as_str = read_line.split
 a = a_as_str.to_i64
-b = b_as_str.to_f64
+b = BigDecimal.new(b_as_str.to_f64)
 
 puts (a * (b * 100).to_i64) // 100
