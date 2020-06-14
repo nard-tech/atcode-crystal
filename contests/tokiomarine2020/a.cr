316576516775a396f3d139873863862715858fd0
Shu Fujita <s.fujita@nard.tech>
2020-06-14 22:51:53 +0900

    [TOKIOMARINE 2020 A] Nickname - Add file

---
+++ contests/tokiomarine2020/a.cr
@@ -0,0 +1,8 @@
+# TOKIOMARINE 2020 A - Nickname
+# https://atcoder.jp/contests/tokiomarine2020/tasks/tokiomarine2020_a
+
+# AC
+# https://atcoder.jp/contests/tokiomarine2020/submissions/14221756
+
+s = read_line
+puts s[0..2]
