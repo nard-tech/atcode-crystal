9c6751d8c09d7fc555f8122428a17600755ea0d2
Shu Fujita <s.fujita@nard.tech>
2020-06-14 23:00:50 +0900

    [TOKIOMARINE 2020 C] Fix file (TLE, Crystal)

--- contests/tokiomarine2020/c.cr
+++ contests/tokiomarine2020/c.cr
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/tokiomarine2020/tasks/tokiomarine2020_c
 
 # TLE
-# https://atcoder.jp/contests/tokiomarine2020/submissions/14242182
+# https://atcoder.jp/contests/tokiomarine2020/submissions/14245066
 
 n, k = read_line.split.map(&.to_i64)
 a = read_line.split.map(&.to_i64)
@@ -14,10 +14,12 @@ class Lamp
   getter :position
   property :power
 
-  def for?(point)
-    # @position - @power - 0.5 <= point <= @position + @power + 0.5
-    # @position - (@power + 0.5) <= point <= @position + (@power + 0.5)
-    (@position - point).abs <= @power
+  def range(n)
+    if power == 0
+      return [position]
+    end
+
+    ([(position - power), 1].max..[(position + power), n].min).to_a
   end
 end
 
@@ -27,7 +29,16 @@ while k > 0
     # puts "break"
     break
   end
-  powers = (1..n).map { |i| lamps.select { |lamp| lamp.for?(i) }.size.to_i64 }
+
+  powers = Array(Int64).new(n, 0_i64)
+
+  lamps.each do |lamp|
+    range = lamp.range(n)
+    range.each do |position|
+      powers[position - 1] = powers[position - 1] + 1
+    end
+  end
+
   lamps.each_with_index do |lamp, i|
     lamp.power = powers[i]
   end
